ac13176c089e9bbfdf89c7b46558518b417c21fb
IAmAnthem <60992751+IAmAnthem@users.noreply.github.com>
2022-08-03 18:47:05 -0400

    Add more real data
    Start linking functions together more cleanly

--- Stat-Canine.ps1
+++ Stat-Canine.ps1
@@ -3,16 +3,30 @@
 Develops a set of functions useful in evaluating a canine
 
 #>
+Function Read-Database {
+    Param()
+    $petDB = Import-CSV -Path .\KNOWNS.csv
+    return $petDB
+    }
+
 function Get-EvalText {
     param ()
-    $evalText = Get-Content -Path .\CertainStat.txt
+    # $evalText = Get-Content -Path .\CertainStat.txt
+    # Ok prompt for some text, we need an input box
+    $evalText = Read-MultiLineInputBoxDialog -Message "Please enter a CERTAIN eval, KNOWN to UNKNOWN" -WindowTitle "Compare Known to Unknown" -DefaultText "Enter some text here..."
+    if ($evalText -eq $null) { Write-Host "You clicked Cancel" }
+    else { 
+        # Don't do anything
+        # Write-Host "You entered the following text: $multiLineText" 
+        }
+    
     return $evalText
 }
 
 function Get-Knowledge {
-    param()
-    $evalText = Get-EvalText
-
+    param(
+    $evalText
+    )
     $knowledgePattern = "think","feel","certain"
     foreach($pattern in $knowledgePattern){
         #DEBUG:         Write-Host "Get-Knowledge is Checking $pattern"
@@ -39,7 +53,8 @@ function Get-Relationship {
 
 function Get-TraitText {
     param(
-    $traitName
+    $traitName,
+    $evalText
     )
     # DEBUG:         Write-Host "Get-TraitText is looking for $traitName"
     $evalText = Get-EvalText
@@ -67,9 +82,10 @@ function Get-TraitText {
 
 function Get-EvalLowRange {
     param(
-    $traitName
+    $traitName,
+    $evalText
     )
-    $traitText = Get-TraitText $traitName
+    # $traitText = Get-TraitText $traitName
     # DEBUG    Write-Host "Get-EvalLowRange is looking for $traitText"
     foreach ($row in $baseArray){
         if($row.evalText -eq $traitText){$evalLowRange = $row.low}	
@@ -79,9 +95,10 @@ function Get-EvalLowRange {
 
 function Get-EvalHighRange {
     param(
-    $traitName
+    $traitName,
+    $traitText
     )
-    $traitText = Get-TraitText $traitName
+    # $traitText = Get-TraitText $traitName
     foreach ($row in $baseArray){
         if($row.evalText -eq $traitText){$evalHighRange = $row.high}	
         }
@@ -90,15 +107,27 @@ function Get-EvalHighRange {
     }
 
 Function Get-KnownPet{
-    param(
-    $name # Would need to add UniqueName param for real usage
-    )
-    foreach ($row in $knownPet){
-        if ($row.Name -eq "Heap ZeroDog"){
-            $knownPet = $row
-            } else {
-            Write-Host "Can't find this pet in database, breaking"
-            break;
+    param()     # No params, we're going to start by asking who you compared to
+    # Develop picker-code
+    $petDB = Read-Database
+    $validChoices = 0..($petDB.Count -1)
+    $choice = ''
+    while ([string]::IsNullOrEmpty($choice)){
+        #thing
+        foreach ($line in $petDB){
+            #another thing
+            Write-Host ('{0} - {1} - {2}' -f $petDB.IndexOf($line),$line.Person,$line.Name)
+            }
+        $choice = Read-Host -Prompt 'Please chose one of the known pets by number'
+        if($choice -in $validChoices){
+            Write-Host (' --- Your choice of: [{0}] is valid.' -f $choice)
+            $knownPet = $petDB[$choice]
+            }
+            else{
+            Write-Warning (' [{0}] is not a valid selection.' -f $choice)
+            Write-Warning '--- TRY AGAIN'
+            pause
+            $choice = ''
             }
         }
     return $knownPet
@@ -142,7 +171,7 @@ Function Get-TraitValueRange {
 
 Function Update-LowPet {
     Param(
-        $name # add $name here to lookup against something other than the zeropet
+        $evalText
     )
     foreach ($traitName in $traits){
         $traitValueHashTable = Get-TraitValueRange $traitName
@@ -167,7 +196,6 @@ Function Update-LowPet {
 
 }
 
-# FIX THIS NEXT
 Function Update-HighPet {
     Param(
         $name # add $name here to lookup against something other than the zeropet
@@ -194,6 +222,68 @@ Function Update-HighPet {
 
 }
 
+function Read-MultiLineInputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText)
+    {
+        Add-Type -AssemblyName System.Drawing
+        Add-Type -AssemblyName System.Windows.Forms
+     
+        # Create the Label.
+        $label = New-Object System.Windows.Forms.Label
+        $label.Location = New-Object System.Drawing.Size(10,10) 
+        $label.Size = New-Object System.Drawing.Size(280,20)
+        $label.AutoSize = $true
+        $label.Text = $Message
+     
+        # Create the TextBox used to capture the user's text.
+        $textBox = New-Object System.Windows.Forms.TextBox 
+        $textBox.Location = New-Object System.Drawing.Size(10,40) 
+        $textBox.Size = New-Object System.Drawing.Size(575,200)
+        $textBox.AcceptsReturn = $true
+        $textBox.AcceptsTab = $false
+        $textBox.Multiline = $true
+        $textBox.ScrollBars = 'Both'
+        $textBox.Text = $DefaultText
+     
+        # Create the OK button.
+        $okButton = New-Object System.Windows.Forms.Button
+        $okButton.Location = New-Object System.Drawing.Size(415,250)
+        $okButton.Size = New-Object System.Drawing.Size(75,25)
+        $okButton.Text = "OK"
+        $okButton.Add_Click({ $form.Tag = $textBox.Text; $form.Close() })
+     
+        # Create the Cancel button.
+        $cancelButton = New-Object System.Windows.Forms.Button
+        $cancelButton.Location = New-Object System.Drawing.Size(510,250)
+        $cancelButton.Size = New-Object System.Drawing.Size(75,25)
+        $cancelButton.Text = "Cancel"
+        $cancelButton.Add_Click({ $form.Tag = $null; $form.Close() })
+     
+        # Create the form.
+        $form = New-Object System.Windows.Forms.Form 
+        $form.Text = $WindowTitle
+        $form.Size = New-Object System.Drawing.Size(610,320)
+        $form.FormBorderStyle = 'FixedSingle'
+        $form.StartPosition = "CenterScreen"
+        $form.AutoSizeMode = 'GrowAndShrink'
+        $form.Topmost = $True
+        $form.AcceptButton = $okButton
+        $form.CancelButton = $cancelButton
+        $form.ShowInTaskbar = $true
+     
+        # Add all of the controls to the form.
+        $form.Controls.Add($label)
+        $form.Controls.Add($textBox)
+        $form.Controls.Add($okButton)
+        $form.Controls.Add($cancelButton)
+     
+        # Initialize and show the form.
+        $form.Add_Shown({$form.Activate()})
+        $form.ShowDialog() > $null   # Trash the text of the button that was clicked.
+     
+        # Return the text that the user entered.
+        return $form.Tag
+    }
+
 <# Placeholder - something like this?, need multiple pets to compare against
     Make a better CSV-replica
     Need to loop through compare-pet until low=high or user says stop
@@ -211,7 +301,9 @@ Function Compare-Pet(
 ########## Setting up vars  #########
 # Playing with hashtables to see how this works
 # Eventually I guess I need to switch to horizontal, since my sourcedata is csv
-# Problem for later!
+# Reminder - source data comes in as NoteProperty with a type of string
+#    Might have some issues with hashtable data being type of int
+#    Problem for later!
 
 # Setting up low pet hashtable, don't even need all traits here
 $lowPet = [ordered]@{    
@@ -240,6 +332,8 @@ $baseArray = @(
 	)
 
 # knownPet replicates the effect of Import-CSV against a normal csv
+# Comment this out and start pulling from a real database
+<#
 $knownPet = @(
     [PSCustomObject]@{
         Name="Heap ZeroDog";
@@ -263,6 +357,7 @@ $knownPet = @(
         Total=0
     }
     )
+#>
 
 $traits = @(
     'Alertness','Appetite','Brutality','Development','Eluding','Energy',
@@ -272,4 +367,21 @@ $traits = @(
 
 # Maybe I'll do a transform with lowpet hashtable to ghostArray csv format?
 $ghostArray = @()
+
+<#
+        Build your program here
+#>
+
+$knownPet = Get-KnownPet
+$evalText = Get-EvalText
+$knowledge = Get-Knowledge $evalText
+
+if($knowledge -ne "certain"){
+    Write-Host "Comparison not certain, breaking run!";break
+    }
+    else{
+    Write-Host "Yup, good eval"
+    }
+
+
 
